a2e9443fdbc4d6ae8b05146eb2d37e672a598d42
Bùi Minh Thái <bmt.uet@gmail.com>
2015-02-24 20:25:04 +0700

    Nhập môn học: click -> hiện trên danh bảng danh sách môn học.

---
+++ js/subject_thaibm.js
@@ -0,0 +1,38 @@
+/**
+ * Created by MinhThai on 2/9/2015.
+ */
+var data= [];
+var subject={name: "", code:"", day:"", start:"", end:""};
+
+function getData(){
+
+    subject.name= document.getElementById("name").value;
+    subject.code= document.getElementById("code").value;
+    subject.day= document.getElementById("day").value;
+    subject.start= document.getElementById("start").value;
+    subject.end= document.getElementById("end").value;
+    if(subject.name ==""){
+        alert("Bạn chưa nhập Tên Môn Học");
+    }
+    else if(subject.code ==""){
+        alert("Bạn chưa nhập Mã Môn Học");
+    }
+    else {
+        data.push(subject) ;
+        //alert(data[0].name + "  " + data[0].code );
+    }
+    printLeftTable();
+
+}
+
+function printLeftTable(){
+    var leftTable="";
+    for(var i=0; i<data.length; i++){
+        leftTable+= "<tr><td><div><b>"+data[i].name+"</b></div>"+"<div >"+data[i].code+"  Thứ "+data[i].day+":  "+data[i].start+"-"+data[i].end+"</div>"+"</td></tr>";
+    }
+    document.getElementById("leftTable").innerHTML= leftTable;
+}
+
+function printRightTable(){
+
+}
