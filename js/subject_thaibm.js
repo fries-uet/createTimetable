3aa6fa93c35ecb703810036b9677c1bc59fb5baa
Bùi Minh Thái <bmt.uet@gmail.com>
2015-02-27 15:44:41 +0700

    Xong chức năng xóa.

--- js/subject_thaibm.js
+++ js/subject_thaibm.js
@@ -1,8 +1,8 @@
 /**
  * Created by MinhThai on 2/9/2015.
  */
-var data= [];
 
+var data= []    //Mảng data[] Lưu danh sách các môn học.
 
 function getData(){
     var subject={name: "", code:"", day:"", start:"", end:""};
@@ -24,15 +24,30 @@ function getData(){
     printLeftTable();
 
 }
-
+//
+//Hàm in danh sách môn học. (Bên trái)
 function printLeftTable(){
     var leftTable="";
     for(var i=0; i<data.length; i++){
-        leftTable+= "<tr><td><div><b>"+data[i].name+"</b></div>"+"<div >"+data[i].code+"  Thứ "+data[i].day+":  "+data[i].start+"-"+data[i].end+"</div>"+"</td></tr>";
+        leftTable+= "<tr><td><button class='close' onclick='deleteLeftSubject(\""+data[i].code+"\");'>×</button><div><b>"+data[i].name+"</b></div><div >"+data[i].code+"  Thứ "+data[i].day+":  "+data[i].start+"-"+data[i].end+"</div>"+"</td></tr>";
     }
     document.getElementById("leftTable").innerHTML= leftTable;
 }
 
+//Hàm xóa môn học trong bảng bên trái
+
+function deleteLeftSubject(code){
+   for(var i=0; i<data.length; i++){
+        if(data[i].code== code){
+            for(var j=i; j<data.length-1; j++){
+                data[j]= data[j+1];
+            }
+            delete data[data.length-1];
+            data.length-=1;
+        }
+   }
+    printLeftTable();
+}
 function printRightTable(){
 
 }
