b4ce3875d6cd3479eb9153adc25eceb1b4b4d5d1
Nguyen Thanh <trungthanh10095@gmail.com>
2015-03-04 09:30:13 +0700

    thanhnv_580 get data from database to create object 'subject'

--- backend/getSubject.php
+++ backend/getSubject.php
@@ -1,3 +1,10 @@
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+</head>
+<body></body>
+</html>
 <?php
 /**
  * Created by PhpStorm.
@@ -5,25 +12,44 @@
  * Date: 03/3/2015
  * Time: 11:25 PM
  */
-
 include("subject.php");
 include("lesson.php");
-
+// thong tin localhost
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "time_table";
 //Tạo các đối tượng subject từ cơ sở dữ liệu
-$s1 = new subject(1, "INT2203", "Công nghệ phần mềm", 3);
-$s2 = new subject(2, "INT2204", "Mạng máy tính", 3);
-$s3 = new subject(3, "MAT1102", "Toán rời rạc", 3);
-$s4 = new subject(4, "PES2206", "Bóng rổ", 1);
-
-//Nhét các subject vào 1 mảng
+// connecting database
+$conn = mysqli_connect($host, $user, $pass, $db);
+if(!$conn){
+    die("error ! < connecting database> ". mysqli_error($conn));
+}
+// get data from database "time_table"
+$get_data = "SELECT subject_id, maMH, tenMH, soTin FROM subject";
+$result = mysqli_query($conn, $get_data);
+// tạo oject "subject" tự động từ database
 $arr = array();
-array_push($arr, $s1);
-array_push($arr, $s2);
-array_push($arr, $s3);
-array_push($arr, $s4);
-
+$sl = mysqli_num_rows($result);
+$i = 0;
+$id = 0;
+$maMH = "";
+$tenMH = "";
+$soTin = 0;
+if($sl>0){
+    while($i<$sl){
+        $row = mysqli_fetch_assoc($result);
+        $id = $row['subject_id'];
+        $maMH = $row['maMH'];
+        $tenMH = $row['tenMH'];
+        $soTin = $row['soTin'];
+        $s = new subject($id, $maMH, $tenMH, $soTin);
+        array_push($arr, $s);
+        $i++;
+    }
+}
 //Nhét mảng trên vào 1 mảng $data theo cấu trúc như bên dưới
 $data = array("data"=>$arr);
-
 //In mã JSON của $data
 print_r(json_encode($data));
+?>
