942e21ae533a400cfabd367e7469b3c7d79973a3
thaibm <bmt.uet@gmail.com>
2015-03-05 20:42:32 +0700

    Demo Nhập môn học.

--- js/subject_thaibm.js
+++ js/subject_thaibm.js
@@ -5,17 +5,24 @@
 var data= []    //Mảng data[] Lưu danh sách các môn học.
 
 function getData(){
-    var subject={name: "", code:"", day:"", start:"", end:""};
-    subject.name= document.getElementById("name").value;
-    subject.code= document.getElementById("code").value;
-    subject.day= document.getElementById("day").value;
-    subject.start= document.getElementById("start").value;
-    subject.end= document.getElementById("end").value;
+    var subject={name: "", code:"",teacher:"", tinChi:"", group:"", day:"", start:"", end:""};
+    subject.name=       document.getElementById("name").value;
+    subject.code=       document.getElementById("code").value;
+    subject.teacher=    document.getElementById("teacher").value;
+    subject.tinChi=     document.getElementById("tinChi").value;
+    subject.group=      document.getElementById("group").value;
+    subject.day=        document.getElementById("day").value;
+    subject.start=      document.getElementById("start").value;
+    subject.end=        document.getElementById("end").value;
+
     if(subject.name ==""){
         alert("Bạn chưa nhập Tên Môn Học");
     }
     else if(subject.code ==""){
         alert("Bạn chưa nhập Mã Môn Học");
+    }
+    else if(subject.teacher ==""){
+        alert("Bạn chưa nhập Tên Giảng Viên");
     }
     else {
         data.push(subject) ;
@@ -29,7 +36,9 @@ function getData(){
 function printLeftTable(){
     var leftTable="";
     for(var i=0; i<data.length; i++){
-        leftTable+= "<tr><td><button class='close' onclick='deleteLeftSubject(\""+data[i].code+"\");'>×</button><div><b>"+data[i].name+"</b></div><div >"+data[i].code+"  Thứ "+data[i].day+":  "+data[i].start+"-"+data[i].end+"</div>"+"</td></tr>";
+        leftTable+= "<tr><td><button class='close' onclick='deleteLeftSubject(\""+data[i].code+"\");'>×</button><div><b>"+data[i].name+"</b></div>";
+        leftTable+= "<div>"+data[i].teacher+"</div>";
+        leftTable+= "<div>"+data[i].code+"  Thứ "+data[i].day+":  "+data[i].start+"-"+data[i].end+"</div>"+"</td></tr>"
     }
     document.getElementById("leftTable").innerHTML= leftTable;
 }
@@ -48,6 +57,11 @@ function deleteLeftSubject(code){
    }
     printLeftTable();
 }
-function printRightTable(){
-
+function ShowGroup(){
+    document.getElementById("showGroup").setAttribute("style", "display: block");
+    document.getElementById("group").innerHTML= "<option>1</option> <option>2</option> <option>3</option>";
+}
+function HideGroup(){
+    document.getElementById("showGroup").setAttribute("style","display: none");
+    document.getElementById("group").innerHTML= "<option>0</option>";
 }
