9f2c6461d283f20736c44086d9524e6514b070ef
Tu TV <tutv95@gmail.com>
2015-03-18 04:03:08 +0700

    Quên chưa thêm 2 file footer.php và header.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="col-lg-12 modal-footer">
+	<span>Copyright @Fries 2015</span>
+</div>
+
+<!-- Javascript -->
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<script src="js/main.js" type="text/javascript"></script>
