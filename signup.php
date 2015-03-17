e829f98c3bc425088bcd960620f6e32853b9376c
Tu TV <tutv95@gmail.com>
2015-03-17 22:56:06 +0700

    Sửa lại form signup

--- signup.php
+++ signup.php
@@ -15,7 +15,7 @@
             <div class="form-group">
                 <label class="control-label col-sm-4" for="name">Tên tài khoản:</label>
                 <div class="col-sm-8">
-                    <input type="text" class="form-control" id="name" placeholder="Tên tài " name="name">
+                    <input type="text" class="form-control" id="name" placeholder="Tên tài khoản" name="name">
                 </div>
             </div>
 
@@ -34,24 +34,24 @@
             </div>
 
             <div class="form-group">
-                <label class="control-label col-sm-4" for="def">Ho:</label>
+                <label class="control-label col-sm-4" for="def">Họ:</label>
                 <div class="col-sm-8">
-                    <input type="email" class="form-control" id="email" placeholder="Ho" name="lastname">
+                    <input type="text" class="form-control" id="lastname" placeholder="Họ và tên đệm" name="lastname">
                 </div>
             </div>
 
             <div class="form-group">
-                <label class="control-label col-sm-4" for="abc">Ten:</label>
+                <label class="control-label col-sm-4" for="abc">Tên:</label>
                 <div class="col-sm-8">
-                    <input type="email" class="form-control" id="email" placeholder="Ten" name="firstname">
+                    <input type="text" class="form-control" id="firstname" placeholder="Tên" name="firstname">
                 </div>
             </div>
 
 
             <div class="form-group">
-                <label class="control-label col-sm-4" for="123">Ngay sinh:</label>
+                <label class="control-label col-sm-4" for="123">Ngày sinh:</label>
                 <div class="col-sm-8">
-                    <input type="email" class="form-control" id="email" placeholder="Ngay sinh" name="birth">
+                    <input type="date" class="form-control" id="birth" placeholder="Ngày sinh" name="birth">
                 </div>
             </div>
 <!--            <div class="form-group">-->
@@ -71,7 +71,6 @@
 <?php
 //    echo $_POST['error'];
 ?>
-
     <script src="js/jquery.min.js"></script>
     <script>
         //Kiểm tra mật khẩu có trùng khớp hay không?
