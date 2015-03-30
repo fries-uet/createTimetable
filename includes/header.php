5a5452a8cc9d64120b41c0ccbcefee03daa9eef0
Tu TV <tutv95@gmail.com>
2015-03-31 02:27:22 +0700

    Fix lai ten va loi php

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,3 @@
-<!
- * Created by PhpStorm.
- * User: TMQ
- * Date: 30/03/2015
- * Time: 12:05 SA
->
-
 <div class="navbar-fixed-top" style="background: #e3e3e3; box-shadow: 1px 1px 5px #101010">
     <h3>
         <a style="padding-left: 10px">
