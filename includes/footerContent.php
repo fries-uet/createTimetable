146ee3d6bb1a8bdfeb938e7155f89ca18707a311
quytm_58 <quytm_58.vnu.edu.vn>
2015-03-31 01:49:49 +0700

    Quý: Thêm Header, Footer trong includes

---
+++ includes/footerContent.php
@@ -0,0 +1,10 @@
+<!
+ * Created by PhpStorm.
+ * User: TMQ
+ * Date: 30/03/2015
+ * Time: 12:22 SA
+>
+
+<div class="modal-footer col-lg-12">
+    <span>Copyright @Fries 2015</span>
+</div>
---
+++ includes/headerContent.php
@@ -0,0 +1,19 @@
+<!
+ * Created by PhpStorm.
+ * User: TMQ
+ * Date: 30/03/2015
+ * Time: 12:05 SA
+>
+
+<div class="navbar-fixed-top" style="background: #e3e3e3; box-shadow: 1px 1px 5px #101010">
+    <h3>
+        <a style="padding-left: 10px">
+            Create Timetable UET - Fries
+        </a>
+        <div class="pull-right">
+            <button class="btn btn-sm btn-info">Sign in</button>        <!-- Đăng nhập-->
+            <button class="btn btn-sm btn-link">Registry</button>       <!-- Đăng kí-->
+            <button class="btn btn-sm btn-link">Help</button>           <!-- Trợ giúp-->
+        </div>
+    </h3>
+</div>
