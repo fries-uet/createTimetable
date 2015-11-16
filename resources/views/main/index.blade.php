a3f9eb6ea46ce75eb90c28ea45bae6550c649601
Tu TV <tutv95@gmail.com>
2015-11-17 00:36:22 +0700

    demo webhook

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -2,7 +2,7 @@
 
 @section ('head.title')
 <title>Create Timetable</title>
-@stop
+@endsection
 
 @section ('body.content')
 <div class="jumbotron">
@@ -10,11 +10,11 @@
 		<h1>Create timetable</h1>
 		<p>Phiên bản beta - Ứng dụng tạo thời khóa biểu cho sinh viên UET :]</p>
 		<p>
-			<a href="{{ route('main.home') }}" class="btn btn-primary btn-lg">Dùng thử 123</a>
+			<a href="{{ route('main.home') }}" class="btn btn-primary btn-lg">Dùng thử</a>
 		</p>
 	</div>
 </div>
 
 @include ('partials.signin')
 @include ('partials.signup')
-@stop
+@endsection
